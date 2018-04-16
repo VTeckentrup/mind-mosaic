3e4acd8e78a1b4b1b6dbfa414bb414060d6b7b46
Jennt11 <jennt11@github.com>
2018-04-16 16:16:07 +0200

    Globals

--- App/src/GlobalDBVars.hx
+++ App/src/GlobalDBVars.hx
@@ -6,6 +6,7 @@ class GlobalDBVars
 {
     public static var _id:Int;
 	public static var _round_ind:Int;
+    public static var level:Int;
     public static var _blue_reward_prob:Float;
     public static var _green_reward_prob:Float;
     public static var _round_reward_prob:Float;
